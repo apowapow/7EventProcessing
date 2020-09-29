e9de175b0a5d1f3357ad84cba62ce5da3341a522
POWER Alexander <71255126+apowapow@users.noreply.github.com>
2020-09-29 15:54:31 +0100

    receive messages

--- main.py
+++ main.py
@@ -2,25 +2,85 @@ import boto3
 import argparse
 import os.path
 import json
-
+import uuid
 
 args = None
 
+S3 = "s3"
+SQS = "sqs"
+SNS = "sns"
+
 FILE_LOCATIONS = "locations.json"
+REGION_NAME = "eu-west-1"
+QUEUE_NAME = "apaltr-{0}".format(str(uuid.uuid4()))
+
+KEY_X = "x"
+KEY_Y = "y"
+KEY_ID = "id"
 
 
 def main():
     session = boto3.Session(
         aws_access_key_id=args.aws_public,
         aws_secret_access_key=args.aws_secret,
+        region_name=REGION_NAME
     )
 
-    s3 = session.resource('s3')
+    s3 = session.resource(S3)
+    sqs = session.client(SQS)
+    sns = session.client(SNS)
 
-    bucket = s3.Bucket(args.bucket_name)
-    json_loc = get_json(bucket, FILE_LOCATIONS)
+    queue = None
+    queue_url = None
+    queue_arn = None
 
-    print(json_loc)
+    try:
+        # Part 1: task 1
+        bucket = s3.Bucket(args.bucket_name)
+        json_loc = get_json(bucket, FILE_LOCATIONS)
+
+        # Part 1: task 2
+        queue = sqs.create_queue(QueueName=QUEUE_NAME)
+        queue_url = sqs.get_queue_url(QueueName=QUEUE_NAME)["QueueUrl"]
+        queue_arn = sqs.get_queue_attributes(QueueUrl=queue_url, AttributeNames=['All'])['Attributes']['QueueArn']
+
+        print("queue_url: {0}".format(queue_url))
+        print("queue_arn: {0}".format(queue_arn))
+
+        policy_document = {
+            'Version': '2012-10-17',
+            'Statement': [{
+                'Sid': f'allow-subscription-{args.topic_arn}',
+                'Effect': 'Allow',
+                'Principal': {'AWS': '*'},
+                'Action': 'SQS:SendMessage',
+                'Resource': f'{queue_arn}',
+                'Condition': {
+                    'ArnEquals': {'aws:SourceArn': f'{args.topic_arn}'}
+                }
+            }]
+        }
+        policy_json = json.dumps(policy_document)
+        sqs.set_queue_attributes(QueueUrl=queue_url, Attributes={"Policy": policy_json})
+
+        subscription = sns.subscribe(
+            TopicArn=args.topic_arn,
+            Protocol=SQS,
+            Endpoint=queue_arn
+        )
+        subscription_arn = subscription['SubscriptionArn']
+
+        while True:
+            response = sqs.receive_message(QueueUrl=queue_url)
+            print(response)
+
+
+    except Exception as e:
+        print("Exception: {0}".format(e))
+
+    finally:
+        if queue and queue_url:
+            sqs.delete_queue(QueueUrl=queue_url)
 
 
 def get_json(bucket, file_name):
@@ -44,6 +104,7 @@ if __name__ == "__main__":
     parser.add_argument('-aws_public', type=str)
     parser.add_argument('-aws_secret', type=str)
     parser.add_argument('-bucket_name', type=str)
+    parser.add_argument('-topic_arn', type=str)
     args = parser.parse_args()
 
     main()
